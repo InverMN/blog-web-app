35cffe997ffd48d564f08628f600a6cf8c4bbdde
Pawel <jankowskipawelxd@gmail.com>
2020-07-07 17:32:37 +0200

    Implement Post list

--- src/components/Hangout.tsx
+++ src/components/Hangout.tsx
@@ -1,13 +1,52 @@
-import React from 'react'
-import { Container, Grid } from '@material-ui/core'
+import React, { Component } from 'react'
+import { Container, Grid, Typography } from '@material-ui/core'
 import { Post } from './index'
+import Axios from 'axios'
 
-export const Hangout: React.FC = () => {
-  return (
-    <div>
-      <Container maxWidth="sm">
-        <Grid container direction="column" spacing={6}></Grid>
-      </Container>
-    </div>
-  )
+interface PostData {
+  id: string
+  author: {
+    id: string
+    username: string
+  }
+  createdAt: number
+  body: string
+}
+
+export class Hangout extends Component {
+  state: {
+    posts: PostData[]
+  } = {
+    posts: [],
+  }
+
+  componentDidMount(): void {
+    console.log('test')
+    Axios.get('http://localhost:5500/api/v1/posts').then((res) => {
+      console.log(res.data)
+      this.setState({ posts: res.data })
+    })
+  }
+
+  render(): JSX.Element {
+    return (
+      <div>
+        <Container maxWidth="xs">
+          <Grid container direction="column" spacing={6}>
+            {this.state.posts === [] ? (
+              <Typography variant="h6">NO POST TO DISPLAY</Typography>
+            ) : (
+              this.state.posts.map((post) => {
+                return (
+                  <Grid item key={post.id}>
+                    <Post {...post} />
+                  </Grid>
+                )
+              })
+            )}
+          </Grid>
+        </Container>
+      </div>
+    )
+  }
 }
