6616f9955a99e0689f164adf937fb9d54b3f2b3c
Pawel <jankowskipawelxd@gmail.com>
2020-07-11 13:00:10 +0200

    Add Class responding of containing  auth status and its access token

---
+++ src/lib/Auth.ts
@@ -0,0 +1,14 @@
+const minute = 1000 * 60
+
+export class Auth {
+  isAuthenticated = false
+  accessToken?: string
+  expiresIn?: number
+
+  set(data: { accessToken: string; refreshToken: string }): void {
+    const { accessToken } = data
+    this.accessToken = accessToken
+    this.expiresIn = Date.now() + minute * 15
+    this.isAuthenticated = true
+  }
+}
