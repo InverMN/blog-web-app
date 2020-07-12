193af9ae515b5522539d5ff080ce3401624e8308
Pawel <jankowskipawelxd@gmail.com>
2020-07-12 15:05:47 +0200

    Add logging without access to one specific account

--- src/components/AppNav.tsx
+++ src/components/AppNav.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { AppBar, Toolbar, IconButton, Typography, Button } from '@material-ui/core'
 import { Menu as MenuIcon } from '@material-ui/icons'
+import { LoginDialog } from './dialogs/index'
 
 export const AppNav: React.FC = () => {
+  const [open, setOpen] = useState(false)
+
+  const handleOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+
   return (
     <div>
       <AppBar>
@@ -11,10 +22,13 @@ export const AppNav: React.FC = () => {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6">Mikroblog</Typography>
-          <Button color="inherit">Login</Button>
+          <Button color="inherit" onClick={handleOpen}>
+            Login
+          </Button>
           <Button color="inherit">Register</Button>
         </Toolbar>
       </AppBar>
+      <LoginDialog onClose={handleClose} open={open} />
     </div>
   )
 }
---
+++ src/components/dialogs/Login.tsx
@@ -0,0 +1,68 @@
+import React, { useContext, useState } from 'react'
+import { UserContext } from '../../contexts/index'
+import { useAPI } from '../../lib/index'
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  useMediaQuery,
+  useTheme,
+  Button,
+  TextField,
+  Backdrop,
+  CircularProgress,
+} from '@material-ui/core'
+
+export interface LoginDialogProps {
+  open: boolean
+  onClose: () => void
+}
+
+export const LoginDialog: React.FC<LoginDialogProps> = (props: LoginDialogProps) => {
+  const { open, onClose } = props
+  const [wait, setWait] = useState(false)
+  const theme = useTheme()
+  const fullscreen = useMediaQuery(theme.breakpoints.down('xs'))
+  const api = useAPI()
+  const { user, setUser } = useContext(UserContext)
+
+  const handleClose = () => {
+    onClose()
+  }
+
+  const handleLogin = () => {
+    setWait(true)
+    api.login('jankowskipawelxd@gmail.com', 'qwaszx').then((res) => {
+      if (setUser !== null) setUser(res.data)
+      console.log('set up user:', user)
+      setWait(false)
+      onClose()
+    })
+  }
+
+  return (
+    <div>
+      <Dialog fullScreen={fullscreen} open={open}>
+        <DialogTitle>Log in</DialogTitle>
+        <DialogContent>
+          <form noValidate>
+            <TextField fullWidth type="email" id="email" label="E-mail" variant="outlined"></TextField>
+            <TextField fullWidth type="password" id="password" label="Password" variant="outlined"></TextField>
+          </form>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} style={{ float: 'left' }} color="primary" autoFocus>
+            Close
+          </Button>
+          <Button disableElevation onClick={handleLogin} color="primary" variant="contained" autoFocus>
+            Login
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <Backdrop style={{ zIndex: 1000000 }} open={wait}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    </div>
+  )
+}
