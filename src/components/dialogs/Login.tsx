e3714a2eb2711a00b584c8012d1648615af8e15a
Pawel <jankowskipawelxd@gmail.com>
2020-07-14 14:39:30 +0200

    Centralize form in dialog

--- src/components/dialogs/Login.tsx
+++ src/components/dialogs/Login.tsx
@@ -13,9 +13,11 @@ import {
   Backdrop,
   CircularProgress,
   Typography,
+  Grid,
 } from '@material-ui/core'
 import { Formik } from 'formik'
 import * as Yup from 'yup'
+import { makeStyles } from '@material-ui/core/styles'
 
 export interface LoginDialogProps {
   open: boolean
@@ -27,12 +29,19 @@ const LoginSchema = Yup.object().shape({
   password: Yup.string().required().min(3).max(200),
 })
 
+const useStyles = makeStyles({
+  fullWide: {
+    width: '100%',
+  },
+})
+
 export const LoginDialog: React.FC<LoginDialogProps> = (props: LoginDialogProps) => {
   const { open, onClose } = props
   const theme = useTheme()
   const fullscreen = useMediaQuery(theme.breakpoints.down('xs'))
   const api = useAPI()
   const { user, setUser } = useContext(UserContext)
+  const classes = useStyles()
 
   const initialValues = {
     email: '',
@@ -68,61 +77,79 @@ export const LoginDialog: React.FC<LoginDialogProps> = (props: LoginDialogProps)
 
   return (
     <div>
-      <Dialog fullScreen={fullscreen} open={open}>
-        <DialogTitle>Log in</DialogTitle>
-        <Formik initialValues={initialValues} validationSchema={LoginSchema} onSubmit={handleLogin}>
-          {({ values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting }) => (
-            <div>
-              <DialogContent>
-                <form noValidate>
-                  <TextField
-                    fullWidth
-                    name="email"
-                    type="email"
-                    id="email"
-                    label="E-mail"
-                    variant="outlined"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.email}
-                  ></TextField>
-                  <Typography color="error">{errors.email && touched.email && errors.email}</Typography>
-                  <TextField
-                    fullWidth
-                    name="password"
-                    type="password"
-                    id="password"
-                    label="Password"
-                    variant="outlined"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.password}
-                  ></TextField>
-                  <Typography color="error"> {errors.password && touched.password && errors.password}</Typography>
-                </form>
-              </DialogContent>
-              <DialogActions>
-                <Button onClick={handleClose} style={{ float: 'left' }} color="primary" autoFocus>
-                  Close
-                </Button>
-                <Button
-                  disableElevation
-                  onClick={() => {
-                    handleSubmit()
-                  }}
-                  color="primary"
-                  variant="contained"
-                  autoFocus
-                >
-                  Login
-                </Button>
-              </DialogActions>
-              <Backdrop style={{ zIndex: 1000000 }} open={isSubmitting}>
-                <CircularProgress variant="indeterminate" color="primary" />
-              </Backdrop>
-            </div>
-          )}
-        </Formik>
+      <Dialog fullScreen={fullscreen} open={open} maxWidth="xs" fullWidth={true}>
+        <Grid container direction="column" justify="center" alignItems="center" style={{ height: '100%' }}>
+          <Grid item>
+            <DialogTitle>Log in</DialogTitle>
+          </Grid>
+          <Grid item className={classes.fullWide}>
+            <Formik initialValues={initialValues} validationSchema={LoginSchema} onSubmit={handleLogin}>
+              {({ values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting }) => (
+                <div>
+                  <DialogContent>
+                    <form noValidate>
+                      <Grid container direction="column" justify="center" alignItems="center">
+                        <Grid item className={classes.fullWide}>
+                          <TextField
+                            fullWidth
+                            name="email"
+                            type="email"
+                            id="email"
+                            label="E-mail"
+                            variant="outlined"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.email}
+                          ></TextField>
+                        </Grid>
+                        <Grid item>
+                          <Typography color="error">{errors.email && touched.email && errors.email}</Typography>
+                        </Grid>
+                        <Grid item className={classes.fullWide}>
+                          <TextField
+                            fullWidth
+                            name="password"
+                            type="password"
+                            id="password"
+                            label="Password"
+                            variant="outlined"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.password}
+                          ></TextField>
+                        </Grid>
+                        <Grid item>
+                          <Typography color="error">
+                            {errors.password && touched.password && errors.password}
+                          </Typography>
+                        </Grid>
+                      </Grid>
+                    </form>
+                  </DialogContent>
+                  <DialogActions>
+                    <Button onClick={handleClose} style={{ float: 'left' }} color="primary" autoFocus>
+                      Close
+                    </Button>
+                    <Button
+                      disableElevation
+                      onClick={() => {
+                        handleSubmit()
+                      }}
+                      color="primary"
+                      variant="contained"
+                      autoFocus
+                    >
+                      Login
+                    </Button>
+                  </DialogActions>
+                  <Backdrop style={{ zIndex: 1000000 }} open={isSubmitting}>
+                    <CircularProgress variant="indeterminate" color="primary" />
+                  </Backdrop>
+                </div>
+              )}
+            </Formik>
+          </Grid>
+        </Grid>
       </Dialog>
     </div>
   )
