3db355995e9b9946cb7b7ec15e1f7ab03e0b4966
Pawel <jankowskipawelxd@gmail.com>
2020-07-16 14:51:05 +0200

    Add Forum Context Provider

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Forum } from './forum/index'
 import { AppNav } from './appbar/index'
+import { ForumContextProvider } from '../contexts/index'
 
 export const Layout: React.FC = () => {
   return (
     <div>
       <AppNav />
-      <Forum />
+      <ForumContextProvider>
+        <Forum />
+      </ForumContextProvider>
     </div>
   )
 }
