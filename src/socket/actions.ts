8831a3f72ebb40da207e895d9e2a783bb5f4e743
Pawel <jankowskipawelxd@gmail.com>
2020-07-30 17:10:45 +0200

    Delete comment

--- src/socket/actions.ts
+++ src/socket/actions.ts
@@ -1,7 +1,6 @@
 import { socket, LoginResults } from './index'
 
 export function connect(accessToken: string): void {
-  console.log('connecting socket')
   socket.emit('login', { accessToken })
   socket.on('login', (results: LoginResults) => {
     if (results.status === 'success') console.log('logged to socket successfully')
