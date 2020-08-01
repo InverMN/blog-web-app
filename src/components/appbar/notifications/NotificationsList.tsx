69226176f51ec903c1743fc461fada31b958f0de
Pawel <jankowskipawelxd@gmail.com>
2020-08-01 14:57:48 +0200

    Add Empty Notifications List

---
+++ src/components/appbar/notifications/NotificationsList.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Popover } from '@material-ui/core'
+
+interface Props {
+  anchorElement: HTMLElement
+  handleClose: () => void
+}
+
+export const NotificationsList: React.FC<Props> = ({ anchorElement, handleClose }) => {
+  return (
+    <Popover
+      open={Boolean(anchorElement)}
+      anchorEl={anchorElement}
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'center',
+      }}
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'center',
+      }}
+      onClose={handleClose}
+    ></Popover>
+  )
+}
