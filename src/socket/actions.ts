b6c7f85141d234ccf7196f48b2bc3499231b928e
Pawel <jankowskipawelxd@gmail.com>
2020-08-02 20:17:26 +0200

    Listen notifications   when logged usccesfully

--- src/socket/actions.ts
+++ src/socket/actions.ts
@@ -4,10 +4,9 @@ import { Notification } from '../contexts/index'
 export function connect(accessToken: string): void {
   socket.emit('login', { accessToken })
   socket.on('login', (results: LoginResults) => {
-    if (results.status === 'success') console.log('logged to socket successfully')
-    else console.log('failed to log to socket')
+    if (results.status === 'success')
+      socket.on('notification', (notificationData: Notification) => handleNewNotification(notificationData))
   })
-  socket.on('notification', (notificationData: Notification) => handleNewNotification(notificationData))
 }
 
 export function disconnect(): void {
