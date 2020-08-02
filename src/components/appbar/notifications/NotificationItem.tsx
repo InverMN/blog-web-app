d6b3d461ee2d2bafa78c4ee0eef2f64ae0926e8e
Pawel <jankowskipawelxd@gmail.com>
2020-08-02 12:59:08 +0200

    Paint unchecked notifications in light blue

--- src/components/appbar/notifications/NotificationItem.tsx
+++ src/components/appbar/notifications/NotificationItem.tsx
@@ -2,20 +2,37 @@ import React from 'react'
 import { ListItem, ListItemText, ListItemAvatar, Avatar } from '@material-ui/core'
 import { Notification as NotificationData } from '../../../contexts/index'
 import Moment from 'moment'
+import { makeStyles } from '@material-ui/core/styles'
 
 interface Props {
   data: NotificationData
 }
 
+const useStyles = makeStyles({
+  unchecked: {
+    backgroundColor: 'rgba(2, 119, 189, 0.25)',
+  },
+})
+
 export const NotificationItem: React.FC<Props> = ({
   data: { id, sender, receiver, subject, data, checked, createdAt },
 }) => {
+  const classes = useStyles()
+
+  let message
+  switch (subject) {
+    case 'REPLIED_POST':
+      message = `${sender.username} replied your post`
+    case 'REPLIED_COMMENT':
+      message = `${sender.username} replied your comment`
+  }
+
   return (
-    <ListItem button>
+    <ListItem button className={checked ? '' : classes.unchecked}>
       <ListItemAvatar>
         <Avatar src={`http://localhost:5500/static/avatars/${sender.id}.png`} />
       </ListItemAvatar>
-      <ListItemText primary={data} secondary={Moment(createdAt).fromNow()} />
+      <ListItemText primary={message} secondary={Moment(createdAt).fromNow()} />
     </ListItem>
   )
 }
