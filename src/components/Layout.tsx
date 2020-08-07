ebb3adc20845abb592647b06635c893017df27a4
Pawel <jankowskipawelxd@gmail.com>
2020-08-07 12:36:41 +0200

    Move Browse Router to Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,14 +2,15 @@ import React from 'react'
 import { AppNav } from './appbar/index'
 import { ForumContextProvider } from '../contexts/index'
 import { PageWrapper } from './pages/PageWrapper'
+import { BrowserRouter } from 'react-router-dom'
 
 export const Layout: React.FC = () => {
   return (
-    <div>
+    <BrowserRouter>
       <AppNav />
       <ForumContextProvider>
         <PageWrapper />
       </ForumContextProvider>
-    </div>
+    </BrowserRouter>
   )
 }
