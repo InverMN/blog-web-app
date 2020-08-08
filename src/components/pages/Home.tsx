d2f2ede7c52d8e8f9c08ba09cdf328ad5ec54597
Pawel <jankowskipawelxd@gmail.com>
2020-08-08 09:49:53 +0200

    Load posts bsed on user context changes

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -2,16 +2,18 @@ import React from 'react'
 import { Forum } from '../forum/index'
 import { ForumContext } from '../../contexts/index'
 import { useAPI } from '../../lib/API'
+import { UserContext } from '../../contexts/UserContext'
 
 export const Home: React.FC = () => {
   const api = useAPI()
   const { dispatch } = React.useContext(ForumContext)
+  const { user } = React.useContext(UserContext)
 
   React.useEffect(() => {
     api.get('posts').then((res) => {
       dispatch({ type: 'LOAD_POSTS', payload: res.data })
     })
-  }, [])
+  }, [user])
 
   return <Forum />
 }
