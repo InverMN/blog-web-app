6187c1b1087b7b48da897d616f8d6e785ef65fdb
Pawel <jankowskipawelxd@gmail.com>
2020-08-14 14:20:15 +0200

    Add abstract login attention dialog

---
+++ src/components/dialogs/MustBeLogged.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Button } from '@material-ui/core'
+
+export const MustBeLogged: React.FC = () => {
+  const [open, setOpen] = React.useState(false)
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+  return (
+    <Dialog open={open} onClose={handleClose}>
+      <DialogTitle>Attempt to use only verified user feature</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          To unlock many actions in this page you must be logged in. You can create new account on use existing one.
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Close</Button>
+      </DialogActions>
+    </Dialog>
+  )
+}
