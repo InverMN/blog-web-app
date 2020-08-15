352b488e0341e03b8c0e0daef633924ce6ab7a44
Pawel <jankowskipawelxd@gmail.com>
2020-08-15 13:02:14 +0200

    Add snackbar after copying

--- src/components/dialogs/Share.tsx
+++ src/components/dialogs/Share.tsx
@@ -8,8 +8,10 @@ import {
   Button,
   InputAdornment,
   IconButton,
+  Snackbar,
 } from '@material-ui/core'
 import { FileCopy as CopyIcon } from '@material-ui/icons'
+import { Alert } from '@material-ui/lab'
 
 interface Props {
   open: boolean
@@ -18,31 +20,49 @@ interface Props {
 }
 
 export const ShareDialog: React.FC<Props> = ({ open, onClose: handleClose, link }) => {
+  const [openCopiedAlert, setOpenCopiedAlert] = React.useState(false)
+
+  const handleOpenCopiedAlert = () => {
+    setOpenCopiedAlert(true)
+  }
+
+  const handleCloseCopiedAlert = () => {
+    setOpenCopiedAlert(false)
+  }
+
   const copyToClipboard = () => {
     navigator.clipboard.writeText(link)
+    handleOpenCopiedAlert()
   }
 
   return (
-    <Dialog open={open} onClose={handleClose}>
-      <DialogTitle>Share link with your friends</DialogTitle>
-      <DialogContent>
-        <TextField
-          variant="outlined"
-          value={link}
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <IconButton onClick={copyToClipboard}>
-                  <CopyIcon />
-                </IconButton>
-              </InputAdornment>
-            ),
-          }}
-        />
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose}>Close</Button>
-      </DialogActions>
-    </Dialog>
+    <div>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Share link with your friends</DialogTitle>
+        <DialogContent>
+          <TextField
+            variant="outlined"
+            value={link}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton onClick={copyToClipboard}>
+                    <CopyIcon />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Close</Button>
+        </DialogActions>
+      </Dialog>
+      <Snackbar open={openCopiedAlert} autoHideDuration={3500} onClose={handleCloseCopiedAlert}>
+        <Alert onClose={handleCloseCopiedAlert} severity="success">
+          Link copied to clipboard
+        </Alert>
+      </Snackbar>
+    </div>
   )
 }
