6083216a0ad209d6b990c0a591353c147fd15014
Pawel <jankowskipawelxd@gmail.com>
2020-08-17 14:29:02 +0200

    Show how many times was report send

--- src/components/appbar/reports/ReportItem.tsx
+++ src/components/appbar/reports/ReportItem.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ListItem, ListItemText, ListItemSecondaryAction, IconButton } from '@material-ui/core'
+import { ListItem, ListItemText, ListItemSecondaryAction, IconButton, Typography } from '@material-ui/core'
 import { Report as ReportData } from '../../../contexts/index'
 import { Cancel as CancelIcon } from '@material-ui/icons'
 import Moment from 'moment'
@@ -28,6 +28,9 @@ export const ReportItem: React.FC<Props> = ({
 
   return (
     <ListItem button onClick={handleClick}>
+      <Typography variant="h4" color="error" style={{ minWidth: 50, textAlign: 'center', marginRight: '8px' }}>
+        {times}
+      </Typography>
       <ListItemText primary={'Content has been reported'} secondary={Moment(createdAt).fromNow()} />
       <ListItemSecondaryAction>
         <IconButton edge="end" onClick={handleDelete}>
