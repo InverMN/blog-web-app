c47098593535569f85792fd4067104f667c76c8e
Pawel <jankowskipawelxd@gmail.com>
2020-08-20 14:57:16 +0200

    Consume context in OfflineServers dialog

--- src/components/dialogs/OfflineServers.tsx
+++ src/components/dialogs/OfflineServers.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import { Dialog, DialogTitle, DialogContent, DialogContentText, Box } from '@material-ui/core'
+import { OfflineServersContext } from '../../contexts/index'
 
-export const OfflineServersDialog: React.FC = () => (
-  <Dialog open={false} fullScreen={true}>
-    <Box style={{ maxWidth: 500, position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
-      <DialogTitle>Ups! Something has broken</DialogTitle>
-      <DialogContent>
-        <DialogContentText>
-          You can&apos;t connect to the server. Try later or report it to our support for e-mail: support@microblog.com
-          . Thanks for all feedback and we apologize for the problems!
-        </DialogContentText>
-      </DialogContent>
-    </Box>
-  </Dialog>
-)
+export const OfflineServersDialog: React.FC = () => {
+  const { offlineServers } = React.useContext(OfflineServersContext)
+
+  return (
+    <Dialog open={offlineServers} fullScreen={true}>
+      <Box style={{ maxWidth: 500, position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
+        <DialogTitle>Ups! Something has broken</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            You can&apos;t connect to the server. Try later or report it to our support for e-mail:
+            support@microblog.com . Thanks for all feedback and we apologize for the problems!
+          </DialogContentText>
+        </DialogContent>
+      </Box>
+    </Dialog>
+  )
+}
